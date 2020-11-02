9bc648555189309d43314ccb9015b5f039a1448f
Tianju Xue <txue@princeton.edu>
2020-11-02 21:40:41 +0800

    Initial commit from local

---
+++ src/tracker.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  
+
+np.set_printoptions(threshold=sys.maxsize, linewidth=1000, suppress=True)
+np.set_printoptions(precision=5)
+np.random.seed(1)
+
+
+if __name__ == '__main__':
+    pass
