9e17e2ee14984f69077075c6674aa311d3ee8105
Tianju Xue <txue@princeton.edu>
2020-11-17 21:31:16 +0800

    Add a sample code for phase field

--- src/tracker.py
+++ src/tracker.py
@@ -1,11 +1,31 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  
+from scipy.io import loadmat
+import h5py
+
 
 np.set_printoptions(threshold=sys.maxsize, linewidth=1000, suppress=True)
 np.set_printoptions(precision=5)
 np.random.seed(1)
 
 
+def load_data():
+    x1 = loadmat('data/mat/calib.mat')
+    x2 = loadmat('data/mat/tippoint.mat')
+    for key, value in x2.items():
+        print(key)
+
+    path_tracer = 'data/mat/tracer_track.mat'
+    arrays = {}
+    f = h5py.File(path_tracer)
+    for k, v in f.items():
+        arrays[k] = np.array(v)
+        print(k)
+
+    print(arrays['tks'].shape)
+
+
 if __name__ == '__main__':
-    pass
+    load_data()
