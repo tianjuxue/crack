cce0e10dc18b6a18ea5bd15153ff480caff761ba
Tianju Xue <txue@princeton.edu>
2020-11-21 14:24:54 +0800

    Test tracker

--- src/tracker.py
+++ src/tracker.py
@@ -1,14 +1,23 @@
 import sys
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  
 from scipy.io import loadmat
 import h5py
+import pandas as pd
+from pandas import DataFrame, Series  
+import pims
+import trackpy as tp
+
 
 
 np.set_printoptions(threshold=sys.maxsize, linewidth=1000, suppress=True)
 np.set_printoptions(precision=5)
 np.random.seed(1)
+
+# mpl.rc('figure',  figsize=(10, 5))
+# mpl.rc('image', cmap='gray')
 
 
 def load_data():
@@ -27,5 +36,22 @@ def load_data():
     print(arrays['tks'].shape)
 
 
+@pims.pipeline
+def gray(image):
+    return image
+
+def test():
+    frames = gray(pims.open('data/png/*.png'))
+    print(frames)
+    print(frames[0][100, :])
+    plt.imshow(frames[0])
+    f = tp.locate(frames[0], diameter=15, invert=False, minmass=1)
+    f.head()
+    tp.annotate(f, frames[0])
+
+
 if __name__ == '__main__':
-    load_data()
+    # load_data()
+    test()
+    plt.show()
+ 
